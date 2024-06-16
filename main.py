d768d1ce531c71a376370bb896370649c833c06d
MoonRacer-GDT <jerothvotal@gmail.com>
2024-06-15 20:07:58 -0700

    updated main.py with parsing and dictionary build fucntions

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     file_path = "books/frankenstein.txt"
     text = read_book(file_path)
     word_count = word_counter(text)
-    print(f"There are {word_count} words.")
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    print(f"There are {word_count} words. Letters have the following frequency:")
+    letter_count = letter_counter(checker(list(text), alpha), build_dictionary(alpha))
+    print(letter_count)
 
 def read_book(book):
     with open(book) as f:
@@ -11,6 +14,24 @@ def read_book(book):
 def word_counter(text):
     words = text.split()
     return len(words)
+
+def build_dictionary(alpha):
+    letter = {}
+    for i in range(26):
+        letter[alpha[i]] = 0
+    return letter
+
+def checker(letter_list, alpha):
+    alpha_list = []
+    for i in letter_list:
+        if i in alpha:
+            alpha_list.append(i)
+    return alpha_list
+
+def letter_counter(letters, dictionary):
+    for l in letters:
+        dictionary[l] += 1
+    return dictionary
 
 if __name__ == "__main__":
     main()
