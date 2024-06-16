60cb0f33e5dc24c189e175d503a47d98db009e28
MoonRacer-GDT <jerothvotal@gmail.com>
2024-06-15 17:47:06 -0700

    updated main, broke function into smaller chunks

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-path_to_file = "books/frankenstein.txt"
 def main():
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    file_path = "books/frankenstein.txt"
+    text = read_book(file_path)
+    word_count = word_counter(text)
+    print(f"There are {word_count} words.")
+
+def read_book(book):
+    with open(book) as f:
+        return f.read()
+
+def word_counter(text):
+    words = text.split()
+    return len(words)
 
 if __name__ == "__main__":
     main()
